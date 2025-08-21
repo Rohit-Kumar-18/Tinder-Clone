d386f9305a7f149dc9b21798bb65592f1e700b37
Rohit-Kumar-18 <rohitk73423@gmail.com>
2025-08-22 01:24:22 +0530

    yes i made some chnages

--- animated.js
+++ animated.js
@@ -1,3 +1,4 @@
+
 function navAnimation() {
             const nav = document.querySelector("nav");
             const subMenus = document.querySelectorAll(".nav-part2 h5 span");
@@ -54,3 +55,65 @@ function navAnimation() {
         }
 
         navAnimation();
+
+
+function page2Animation() {
+    var rightElems=document.querySelectorAll(".right-elem")
+        rightElems.forEach(function(elem){
+           elem.addEventListener("mouseenter",function(){
+
+            //  console.log(elem.getBoundingClientRect())
+
+            gsap.to(elem.childNodes[3],{
+                opacity:1,
+                scale:1
+            })
+           })
+           elem.addEventListener("mouseleave",function(){
+             gsap.to(elem.childNodes[3],{
+                opacity:0,
+                scale:0
+             })
+           })
+           elem.addEventListener("mousemove",function(dets){
+            gsap.to(elem.childNodes[3],{
+                x:dets.x - elem.getBoundingClientRect().x-20,
+                y:dets.y - elem.getBoundingClientRect().y-100
+            })
+           })
+        })
+}
+page2Animation()
+
+var sections = document.querySelectorAll(".sec-right")
+sections.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        elem.childNodes[3].style.opacity = 1
+        elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave", function(){
+        elem.childNodes[3].style.opacity = 1
+        elem.childNodes[3].load()
+    })
+})
+
+// var tl=gsap.timeline()
+// tl.from("#page1",{
+//     opacity:0,
+//     // duration:0.3,
+//     // delay:0.2
+// })
+// tl.from ("#page1",{
+//     transform:"scaleX(0.1) scaleY(0.2)",
+//     borderRadius:"50px",
+//     // duration:2,
+//     ease:"ease-out"
+// })
+// tl.from("#nav",{
+//     opacity:0
+// })
+// tl.from("#page1 h1, #page1 p, #page1 div",{
+//     opacity:0,
+//     duration:0.5,
+//     stagger:0.2
+// })
